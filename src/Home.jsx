26556c6c47af266756443ac41c26fe86535e62ff
Himanshu Sharma <32563057+hunnysharma@users.noreply.github.com>
2020-11-11 19:54:49 +0530

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Common from './Common'
+import img2 from './images/img2.png'
+const Home = () => {
+  return (
+    <>
+     <Common
+     imgsrc = {img2}
+     btn = 'Get Started'
+     visit = './service'
+      />
+    </>
+  )
+}
+
+export default Home
---
+++ src/Sdata.js
@@ -0,0 +1,36 @@
+import a11 from './images/a11.png'
+import a12  from './images/a12.png'
+import a13  from './images/a13.png'
+import a14  from './images/a14.png'
+import a15  from './images/a15.png'
+import a16 from './images/a16.jpg'
+
+
+
+const Sdata = [
+    {
+        imgSrc : a11,
+        title : 'Infinite Scroll Gallery'
+    },
+    {
+        imgSrc : a12,
+        title : 'React Photo Search'
+    },
+    {
+        imgSrc : a13,
+        title : 'Fun With Pure Javascript'
+    },
+    {
+        imgSrc : a14,
+        title : 'BLog For A Company'
+    },
+    {
+        imgSrc : a15,
+        title : 'E Learning Website'
+    },
+    {
+        imgSrc : a16,
+        title : 'App Development Using Flutter'
+    },
+]
+export default Sdata;
