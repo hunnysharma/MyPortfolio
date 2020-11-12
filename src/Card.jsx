624a4eea518f967b2f9801d66d055b1b53b966c8
Himanshu Sharma <32563057+hunnysharma@users.noreply.github.com>
2020-11-12 20:21:11 +0530

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -11,7 +11,7 @@ const Card = (props) => {
                     <div className="card-body">
                         <h5 className="card-title">{props.title}</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <NavLink to="/contact" className="btn btn-primary">Live Demo</NavLink>
+                        <a href={props.link} target="_blank" className="btn btn-primary">Live Demo</a>
                     </div>
                 </div>
             </div>
