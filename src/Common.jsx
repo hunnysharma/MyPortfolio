e0351ad50149a9d036e607deb0d3e1248f648749
Himanshu Sharma <32563057+hunnysharma@users.noreply.github.com>
2020-12-14 12:31:19 +0530

    Update Common.jsx

--- src/Common.jsx
+++ src/Common.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 const Common = (props) => {
   return (
     <>
-      <section id='header' className = 'd-flex align-items-center'>
+      <section id='header' className = 'd-flex align-items-center hero'>
         <div className='container-fluid' >
           <div className='row' >
             <div className='col-10 mx-auto'>
